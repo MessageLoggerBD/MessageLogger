8acac7f3edc516c7f1bcb9367b14fe4e6c143def
Mirco Wittrien <mirco.wittrien@gmail.com>
2020-01-17 19:52:22 +0100

    Update MessageLogger.plugin.js

--- MessageLogger.plugin.js
+++ MessageLogger.plugin.js
@@ -70,7 +70,7 @@ class MessageLogger {
 	}
 
 	getSettingsPanel () {
-		if (!global.BDFDB || typeof BDFDB != "object" || !BDFDB.loaded || !this.started) return;
+		if (!window.BDFDB || typeof BDFDB != "object" || !BDFDB.loaded || !this.started) return;
 		let settingspanel, settingsitems = [];
 		
 		settingsitems.push(BDFDB.ReactUtils.createElement(BDFDB.LibraryComponents.Flex, {
@@ -124,8 +124,8 @@ class MessageLogger {
 	load () {}
 
 	start () {
-		if (!global.BDFDB) global.BDFDB = {myPlugins:{}};
-		if (global.BDFDB && global.BDFDB.myPlugins && typeof global.BDFDB.myPlugins == "object") global.BDFDB.myPlugins[this.getName()] = this;
+		if (!window.BDFDB) window.BDFDB = {myPlugins:{}};
+		if (window.BDFDB && window.BDFDB.myPlugins && typeof window.BDFDB.myPlugins == "object") window.BDFDB.myPlugins[this.getName()] = this;
 		var libraryScript = document.querySelector('head script#BDFDBLibraryScript');
 		if (!libraryScript || (performance.now() - libraryScript.getAttribute("date")) > 600000) {
 			if (libraryScript) libraryScript.remove();
@@ -134,18 +134,18 @@ class MessageLogger {
 			libraryScript.setAttribute("type", "text/javascript");
 			libraryScript.setAttribute("src", "https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDFDB.min.js");
 			libraryScript.setAttribute("date", performance.now());
-			libraryScript.addEventListener("load", () => {this.initialize();});
+			libraryScript.addEventListener("load", _ => {this.initialize();});
 			document.head.appendChild(libraryScript);
 		}
-		else if (global.BDFDB && typeof BDFDB === "object" && BDFDB.loaded) this.initialize();
-		this.startTimeout = setTimeout(() => {
+		else if (window.BDFDB && typeof BDFDB === "object" && BDFDB.loaded) this.initialize();
+		this.startTimeout = setTimeout(_ => {
 			try {return this.initialize();}
 			catch (err) {console.error(`%c[${this.getName()}]%c`, "color: #3a71c1; font-weight: 700;", "", "Fatal Error: Could not initiate plugin! " + err);}
 		}, 30000);
 	}
 
 	initialize () {
-		if (global.BDFDB && typeof BDFDB === "object" && BDFDB.loaded) {
+		if (window.BDFDB && typeof BDFDB === "object" && BDFDB.loaded) {
 			if (this.started) return;
 			BDFDB.PluginUtils.init(this);
 
@@ -173,7 +173,7 @@ class MessageLogger {
 	}
 
 	stop () {
-		if (global.BDFDB && typeof BDFDB === "object" && BDFDB.loaded) {
+		if (window.BDFDB && typeof BDFDB === "object" && BDFDB.loaded) {
 			this.stopping = true;
 
 			BDFDB.PluginUtils.clear(this);
@@ -252,7 +252,7 @@ class MessageLogger {
 
 	writeLog (filepath, message) {
 		if (!this.loggerqueues[message.channel_id]) this.loggerqueues[message.channel_id] = {queue:[], running:false};
-		var runQueue = () => {
+		var runQueue = _ => {
 			if (!this.loggerqueues[message.channel_id].running) {
 				let logmessage = this.loggerqueues[message.channel_id].queue.shift();
 				if (logmessage) {
